bca507b237fa6e2eaee1ba49b95177bef4d9d6f8
Xellos <jakub.safin@gmail.com>
2017-11-15 12:38:02 +0100

    changed input to cmdline arg

--- fks/linter/__init__.py
+++ fks/linter/__init__.py
@@ -70,7 +70,7 @@ class Parser:
         self.context = Context()
 
         for line in stream:
-            line = line[:-1] # TODO CRLF
+            line = line[:-1]
 
             self.state = self.STATE_TEXT
             self.context.lineno += 1
